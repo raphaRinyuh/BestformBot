d55390f31c69ce18c9d6bd8c0efe607a55f05192
Raphael <raphael.albers04@gmx.de>
2024-02-02 18:50:37 +0100

    chore: added endpoint method types

---
+++ src/types/endpointTypes.ts
@@ -0,0 +1,6 @@
+export type endpointMethod = GetEndpointMethod | PostEndpointMethod | PutEndpointMethod | DeleteEndpointMethod;
+
+export type GetEndpointMethod = 'GET';
+export type PostEndpointMethod = 'POST';
+export type PutEndpointMethod = 'PUT';
+export type DeleteEndpointMethod = 'DELETE';
