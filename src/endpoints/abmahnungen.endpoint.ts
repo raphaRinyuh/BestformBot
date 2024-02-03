08440d14109efe5186848a475a7dffb800a982e1
Raphael <raphael.albers04@gmx.de>
2024-02-03 11:58:43 +0100

    refactor: Endpoint strucutre is now in methods instead of typical api structure

--- src/endpoints/abmahnungen.endpoint.ts
+++ src/endpoints/abmahnungen.endpoint.ts
@@ -1,73 +1,35 @@
 /* eslint-disable indent */
-import { ObjectId } from 'mongodb';
-import { collections } from 'src/services/database.service';
-import Abmahnung from 'src/models/abmahnungen.model';
-import { endpointMethod } from 'src/types/endpointTypes';
-import { DbResponse } from 'src/types/dbResponse';
 
-export async function abmahnungEndpoint(method: endpointMethod, data?: object): Promise<DbResponse | undefined> {
-	switch (method) {
-		case 'GET': {
-			return await get();
-		}
-		case 'POST': {
-			if (!data) return;
-			return await post(data as Abmahnung);
-		}
-		case 'PUT': {
-			return await put();
-		}
-		case 'DELETE': {
-			return await del();
-		}
+import Abmahnung from '../models/abmahnungen.model';
+import { collections } from '../services/database.service';
+import { DbResponse } from '../types/dbResponse';
+
+export async function getAllAbmahnungen(): Promise<DbResponse> {
+	const data = await collections.abmahnungen?.find({});
+
+	if (!data) {
+		return { response: 'There was an error fetching data from the abmahnungen collection', status: 'error' };
 	}
+
+	return { status: 'success', response: data?.toArray() };
 }
 
-async function get(id?: Uint8Array): Promise<DbResponse> {
-	if (!collections.abmahnungen) {
-		return {
-			response: 'Connection to collection: abmahnung could not be established',
-			status: 'error',
-		};
+export async function countUserAbmahnungen(userId: string): Promise<DbResponse> {
+	const data = await collections.abmahnungen?.find({ target: userId });
+	if (!data) {
+		return { response: 'There was an error fetching data from the abmahnungen collection', status: 'error' };
 	}
-	if (id) {
-		const abmahnung = await collections.abmahnungen.findOne({ _id: new ObjectId(id) });
-		if (abmahnung) {
-			return { response: abmahnung.toJSON(), status: 'success' };
-		}
-		else {
-			return { response: 'There was an error while retrieving the Abmahnung', status: 'error' };
-		}
-	}
-	else {
-		const abmahnungen = await collections.abmahnungen.find({});
-		if (abmahnungen) {
-			return { response: JSON.stringify(abmahnungen.toArray()), status: 'success' };
-		}
-		else {
-			return { response: 'There was an error while retrieving the Abmahnung', status: 'error' };
-		}
-	}
-}
-async function post(data: Abmahnung): Promise<DbResponse> {
-	if (!collections.abmahnungen) {
-		return {
-			response: 'Connection to collection: abmahnung could not be established',
-			status: 'error',
-		};
-	}
-	const result = await collections.abmahnungen.insertOne(data);
 
-	if (result) {
-		return { response: `Created new abmahnung with id ${result.insertedId}`, status: 'success' };
+	return { status: 'success', response: (await data.toArray()).length };
+}
+
+export async function createNewAbmahnung(abmahnung: Abmahnung): Promise<DbResponse> {
+	const data = await collections.abmahnungen?.insertOne(abmahnung);
+
+	if (!data) {
+		return { response: 'There was an error creating data in the abmahnungen collection', status: 'error' };
 	}
-	else {
-		return { response: 'There was an error while creating a Abmahnung', status: 'error' };
-	}
+
+	return { status: 'success', response: 'created new abmahnung' };
 }
-async function put(): Promise<DbResponse> {
-	throw Error;
-}
-async function del(): Promise<DbResponse> {
-	throw Error;
-}
+
