13806e9be2c4f4649f90ac84a540dca775ef12d7
Raphael <raphael.albers04@gmx.de>
2024-02-03 11:59:01 +0100

    fix: removed id field from Abmahnung model

--- src/models/abmahnungen.model.ts
+++ src/models/abmahnungen.model.ts
@@ -1,14 +1,10 @@
-import { ObjectId } from 'mongodb';
-
 export default class Abmahnung {
-	private id: Uint8Array;
 	public target: string;
 	public reason: string;
 	public createdBy: string;
 	public createdAt: Date;
 
 	constructor(target: string, reason: string, createdBy: string, createdAt: Date) {
-		this.id = ObjectId.generate();
 		this.target = target;
 		this.reason = reason;
 		this.createdAt = createdAt;
