7e1ba5e6a4120c7d2854da84583964bcd35879b6
Raphael <raphael.albers04@gmx.de>
2024-02-03 15:59:42 +0100

    feature: Added sendAbmahnungenRankingMessage method

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 import { setClientCommands } from './Commands';
+import { sendAbmahnungenRankingMessage } from './embeds/abmahnungRanking.embed';
 import connectToMongoDatabase from './services/database.service';
 import { handleSlashCommand } from './services/handleSlashCommand.service';
-
 dotenv.config();
 
 const bot = new Client({
@@ -18,8 +18,8 @@ const bot = new Client({
 bot.once(Events.ClientReady, async readyClient => {
 	await connectToMongoDatabase();
 	console.log(`logged in as ${readyClient.user.tag}`);
+	sendAbmahnungenRankingMessage(bot);
 });
-
 setClientCommands(bot);
 handleSlashCommand(bot);
 
