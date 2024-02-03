1906f03ec33263eec091c5093ff2098b796876f3
Raphael <raphael.albers04@gmx.de>
2024-02-03 12:00:59 +0100

    refactor: removed the slashCommandHandling to different file. revert: removed the test of databse connection

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
-import { Client, Events, GatewayIntentBits, Interaction } from 'discord.js';
 import { setClientCommands } from './Commands';
-import { connectToMongoDatabase } from './services/database.service';
-import { abmahnungEndpoint } from './endpoints/abmahnungen.endpoint';
-
-connectToMongoDatabase();
-const data = abmahnungEndpoint('GET');
-
-console.log(data);
+import connectToMongoDatabase from './services/database.service';
+import { handleSlashCommand } from './services/handleSlashCommand.service';
 
 dotenv.config();
-
-console.log('Client is starting...');
 
 const bot = new Client({
 	intents: [
@@ -23,31 +16,11 @@ const bot = new Client({
 });
 
 bot.once(Events.ClientReady, async readyClient => {
+	await connectToMongoDatabase();
 	console.log(`logged in as ${readyClient.user.tag}`);
 });
 
 setClientCommands(bot);
-bot.on(Events.InteractionCreate, async (interaction: Interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No Command matching ${interaction.commandName} found`);
-	}
-
-	try {
-		await command?.execute(interaction);
-	}
-	catch (e: unknown) {
-		console.error(e);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+handleSlashCommand(bot);
 
 bot.login(process.env.BOT_TOKEN);
