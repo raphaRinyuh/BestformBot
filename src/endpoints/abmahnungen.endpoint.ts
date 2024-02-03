c6469e41ea9f48cbc6dc6fb7c5df0f3777d76309
Raphael <raphael.albers04@gmx.de>
2024-02-03 15:59:17 +0100

    fix: adapted DbResponse to use generic type for response

--- src/endpoints/abmahnungen.endpoint.ts
+++ src/endpoints/abmahnungen.endpoint.ts
@@ -1,20 +1,18 @@
-/* eslint-disable indent */
-
 import Abmahnung from '../models/abmahnungen.model';
 import { collections } from '../services/database.service';
 import { DbResponse } from '../types/dbResponse';
 
-export async function getAllAbmahnungen(): Promise<DbResponse> {
+export async function getAllAbmahnungen(): Promise<DbResponse<unknown>> {
 	const data = await collections.abmahnungen?.find({});
 
 	if (!data) {
 		return { response: 'There was an error fetching data from the abmahnungen collection', status: 'error' };
 	}
 
-	return { status: 'success', response: data?.toArray() };
+	return { status: 'success', response: await data?.toArray() };
 }
 
-export async function countUserAbmahnungen(userId: string): Promise<DbResponse> {
+export async function countUserAbmahnungen(userId: string): Promise<DbResponse<unknown>> {
 	const data = await collections.abmahnungen?.find({ target: userId });
 	if (!data) {
 		return { response: 'There was an error fetching data from the abmahnungen collection', status: 'error' };
@@ -23,7 +21,7 @@ export async function countUserAbmahnungen(userId: string): Promise<DbResponse> 
 	return { status: 'success', response: (await data.toArray()).length };
 }
 
-export async function createNewAbmahnung(abmahnung: Abmahnung): Promise<DbResponse> {
+export async function createNewAbmahnung(abmahnung: Abmahnung): Promise<DbResponse<unknown>> {
 	const data = await collections.abmahnungen?.insertOne(abmahnung);
 
 	if (!data) {
@@ -33,3 +31,23 @@ export async function createNewAbmahnung(abmahnung: Abmahnung): Promise<DbRespon
 	return { status: 'success', response: 'created new abmahnung' };
 }
 
+export async function getAbmahnungRanking(): Promise<DbResponse<string | Map<string, number>>> {
+	const data = await collections.abmahnungen?.find({}).toArray();
+	const arr = new Map<string, number>();
+	data?.forEach((v) => {
+		if (arr.has(v.target)) {
+			const temp = arr.get(v.target);
+			//	@ts-expect-error temp cannot be empty at this point
+			arr.set(v.target, temp + 1);
+		}
+		else {
+			arr.set(v.target, 1);
+		}
+	});
+
+	if (!data) {
+		return { response: 'A Abmahnungen Ranking could not be established', status: 'error' };
+	}
+	const mapSort1 = new Map([...arr.entries()].sort((a, b) => b[1] - a[1]));
+	return { status: 'success', response: mapSort1 };
+}
